2b22e08c3fa02102a353ca0df341f6fd3435af8a
Rahulx0 <rahulsharma1234h@gmail.com>
2026-02-06 15:28:58 +0530

    Added frontend files

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import Signin from "./pages/Signin"
 import Homepage from "./pages/Homepage"
 import Navbar from "./components/Navbar"
 import Profile from "./pages/Profile"
+import AuthPrivate from "./components/AuthPrivate"
 function App() {
 
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route element={<AuthPrivate />}>
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
+        </Route>
+      </Routes>
+      <Routes>
+        
         <Route path="/" element={<Homepage />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
